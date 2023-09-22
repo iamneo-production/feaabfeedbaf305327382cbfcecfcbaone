884701de1dc5a0f8764748f825b616b2b69f71da
Iamneo <techadmin@iamneo.ai>
2023-09-22 14:28:04 +0000

    Backup Commit

--- test.tf
+++ test.tf
@@ -5,9 +5,16 @@ terraform{
       }
     }
     provide "aws"{
-        region=""
-        access_key=""
-        secret_key=""
+        region="ap-northeast-1"
+        access_key="AKIAVZWTJB733DHSWOHU"
+        secret_key="Cia4M/UV5O/x+4UX3UZBLLaX48vgmQXF+SEX9VTz"
+    }
+    resource "aws_instance" "example"{
+        ami="ami-0261755bbcb8c4a84"
+        instance_type="t2.micro"
+    }
+    output "public_ip"{
+        value=aws_instance
     }
     
 }
